1d5bc0f71823aa0a845353008f49f7366a34054a
Raisul Islam <raisul.me@gmail.com>
2023-11-26 19:25:47 +0600

    Update pagination.blade.php

--- resources/views/tailwind/includes/partials/pagination.blade.php
+++ resources/views/tailwind/includes/partials/pagination.blade.php
@@ -1,25 +1,3 @@
-{{-- <div>
-    @if ($paginator->hasPages())
-        <nav role="navigation" aria-label="Pagination Navigation">
-            <span>
-                @if ($paginator->onFirstPage())
-                    <span>Previous</span>
-                @else
-                    <button wire:click="previousPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" rel="prev">Previous</button>
-                @endif
-            </span>
- 
-            <span>
-                @if ($paginator->onLastPage())
-                    <span>Next</span>
-                @else
-                    <button wire:click="nextPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" rel="next">Next</button>
-                @endif
-            </span>
-        </nav>
-    @endif
-</div> --}}
-
 <div>
     @if ($paginator->hasPages())
         {{-- @php(isset($this->numberOfPaginatorsRendered[$paginator->getPageName()]) ? $this->numberOfPaginatorsRendered[$paginator->getPageName()]++ : $this->numberOfPaginatorsRendered[$paginator->getPageName()] = 1) --}}
