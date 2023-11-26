571290ff803626f5d22cd6f5d92044883c202058
Raisul Islam <raisul.me@gmail.com>
2023-11-26 19:03:50 +0600

    Update pagination.blade.php

--- resources/views/tailwind/includes/partials/pagination.blade.php
+++ resources/views/tailwind/includes/partials/pagination.blade.php
@@ -5,7 +5,7 @@
                 @if ($paginator->onFirstPage())
                     <span>Previous</span>
                 @else
-                    <button wire:click="previousPage" wire:loading.attr="disabled" rel="prev">Previous</button>
+                    <button wire:click="previousPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" rel="prev">Previous</button>
                 @endif
             </span>
  
@@ -13,7 +13,7 @@
                 @if ($paginator->onLastPage())
                     <span>Next</span>
                 @else
-                    <button wire:click="nextPage" wire:loading.attr="disabled" rel="next">Next</button>
+                    <button wire:click="nextPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" rel="next">Next</button>
                 @endif
             </span>
         </nav>
