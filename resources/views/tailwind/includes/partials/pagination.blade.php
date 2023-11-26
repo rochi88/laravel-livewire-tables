51f1150fad9577636d130be36ef51711ce2b610d
Raisul Islam <raisul.me@gmail.com>
2023-11-26 19:00:13 +0600

    Update pagination.blade.php

--- resources/views/tailwind/includes/partials/pagination.blade.php
+++ resources/views/tailwind/includes/partials/pagination.blade.php
@@ -1,5 +1,25 @@
 <div>
     @if ($paginator->hasPages())
+        <nav role="navigation" aria-label="Pagination Navigation">
+            <span>
+                @if ($paginator->onFirstPage())
+                    <span>Previous</span>
+                @else
+                    <button wire:click="previousPage" wire:loading.attr="disabled" rel="prev">Previous</button>
+                @endif
+            </span>
+ 
+            <span>
+                @if ($paginator->onLastPage())
+                    <span>Next</span>
+                @else
+                    <button wire:click="nextPage" wire:loading.attr="disabled" rel="next">Next</button>
+                @endif
+            </span>
+        </nav>
+    @endif
+
+    {{-- @if ($paginator->hasPages())
         @php(isset($this->numberOfPaginatorsRendered[$paginator->getPageName()]) ? $this->numberOfPaginatorsRendered[$paginator->getPageName()]++ : $this->numberOfPaginatorsRendered[$paginator->getPageName()] = 1)
 
         <nav role="navigation" aria-label="Pagination Navigation" class="flex items-center justify-between">
@@ -112,5 +132,5 @@
                 </div>
             </div>
         </nav>
-    @endif
+    @endif --}}
 </div>
