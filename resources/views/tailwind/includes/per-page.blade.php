db06fca1d9a716d4dbaf84fe19d4e5dcdf458874
Raisul Islam <raisul.me@gmail.com>
2023-11-26 19:22:26 +0600

    Update per-page.blade.php

--- resources/views/tailwind/includes/per-page.blade.php
+++ resources/views/tailwind/includes/per-page.blade.php
@@ -1,7 +1,7 @@
 @if ($paginationEnabled && $showPerPage)
     <div class="w-full md:w-auto ml-0 md:ml-2">
         <select
-            wire:model="perPage"
+            wire:model.live="perPage"
             id="perPage"
             class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out sm:text-sm sm:leading-5 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:text-white dark:border-gray-600"
         >
